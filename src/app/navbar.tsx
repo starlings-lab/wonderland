8739286e81212a9dc629119701c0bff0afa3f759
vnadoda <vipul@carapace.finance>
2023-12-05 16:57:38 -0600

    Update home screen style

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,24 +2,26 @@
 
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
-
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { Progress } from "@/components/ui/progress";
+import { NavigationMenuContent } from "@radix-ui/react-navigation-menu";
 
 export default function NavBar() {
   return (
     <>
-      <NavigationMenu className="p-3">
+      <NavigationMenu className="p-5">
         <NavigationMenuList>
           <NavigationMenuItem>
             <Link href="/" passHref>
-              WONDERLAND
+              <p className="nav-home">WONDERLAND</p>
             </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Progress value={50} />
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,24 +12,24 @@ import { cn } from "@/lib/utils";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <Card className="max-w-lg">
+      <Card className="max-w-lg2">
         <CardHeader>
           <CardTitle>
-            <p className="scroll-m-20 text-3xl tracking-tight">
+            <p className="scroll-m-20 text-3xl tracking-tight home">
               The best way to learn
             </p>
-            <p className="scroll-m-20 text-3xl tracking-tight text-teal-500">
+            <p className="scroll-m-20 text-3xl tracking-tight text-teal-500 home">
               Crypto and DeFi
             </p>
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <CardDescription>
+          <CardDescription className="home-desc">
             Learn by doing with interactive playgrounds and tutorials.
           </CardDescription>
         </CardContent>
         <CardFooter>
-          <Button className="bg-emerald-600 text-white">Get Started</Button>
+          <Button className="bg-[#179E7E] text-white">Get Started</Button>
         </CardFooter>
       </Card>
     </main>
