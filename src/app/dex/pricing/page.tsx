6c325b4e4c6ac10aeb280758ccce0db4b719cfa9
vnadoda <vipul@carapace.finance>
2023-12-11 17:43:49 -0600

    Completed pricing chapter
    - Created Quiz component
    - Added review page for pricing

--- src/app/dex/pricing/page.tsx
+++ src/app/dex/pricing/page.tsx
@@ -7,6 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { AppContext } from "@/app/contexts/AppContextProvider";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import MultipleChoiceQuiz from "@/components/MultipleChoiceQuiz";
 
 const Pricing: React.FC = () => {
   const router = useRouter();
@@ -77,7 +78,7 @@ const Pricing: React.FC = () => {
                 exchange(in the next chapter, we'll discuss where this pool of
                 capital comes from.)
               </div>
-              <div className="flex justify-center p-1 mt-5 mb-5">
+              <div className="flex justify-center p-1 mt-5">
                 <Image
                   src="/images/pricing-eth-dai.png"
                   alt="Image"
@@ -85,15 +86,112 @@ const Pricing: React.FC = () => {
                   height={178}
                 />
               </div>
+              <div className="mt-5">
+                In the example above, there are 10 ETH and 2000 USDC that can be
+                used for exchange in Uniswap.
+              </div>
+              <div className="mt-5">
+                Uniswap makes sure that they always get the constant value when
+                they multiply the quantity of ETH and USDC. In this case, they
+                make sure that it is always 20,000.
+              </div>
+              <div className="mt-5">
+                In this case, they make sure that it is always 20,000.
+              </div>
             </div>
           )}
-          {currentQuestion < totalQuestions && currentQuestion !== 3 && (
-            <div className="flex flex-row justify-center items-center">
-              <Button className="mt-5" onClick={onContinue}>
-                Continue
-              </Button>
+          {currentQuestion >= 3 && (
+            <div>
+              <div className="mt-5">
+                Let’s imagine the case where Alice wants to buy USDC for 1 ETH.
+              </div>
+              <div className="flex justify-center p-1 mt-5 mb-5">
+                <Image
+                  src="/images/pricing-uniswap.png"
+                  alt="Image"
+                  width={286}
+                  height={178}
+                />
+              </div>
+              <div className="mt-5">
+                She sells 1 ETH to the exchange. Then the exchange has 11 ETH
+                and 2000 USDC.
+              </div>
+              <div className="mt-5">
+                However, at this point, 11 * 2000 != 20000.
+              </div>
+              <div className="mt-5">
+                So the exchange needs to change the quantity of USDC they have.
+              </div>
+              <div className="mt-5">
+                If the quantity of USDC is x, the exchange wants to make sure
+                that: 11 * x = 20000
+              </div>
+              <MultipleChoiceQuiz
+                className="mt-5"
+                question="This means:"
+                answers={["x = 1818", "x = 2000", "x = 20000", "x = 182"]}
+                correctAnswerIndex={0}
+                onCorrectAnswer={onContinue}
+                onSkip={onContinue}
+              />
             </div>
           )}
+          {currentQuestion >= 4 && (
+            <div>
+              <div className="mt-5">
+                Since x = 20000 / 11, the x, the quantity of USDC in the
+                exchange should be approximately 1818.
+              </div>
+
+              <div className="mt-5">
+                Now, since we know that we need to keep only 1818 in the
+                exchange, we can send some USDC back to Alice.
+              </div>
+              <MultipleChoiceQuiz
+                className="mt-5"
+                question="Alice will get:"
+                answers={["1818 USDC", "182 USDC", "100 USDC", "11 USDC"]}
+                correctAnswerIndex={1}
+                onCorrectAnswer={onContinue}
+                onSkip={onContinue}
+              />
+            </div>
+          )}
+          {currentQuestion >= 5 && (
+            <div>
+              <div className="mt-5">
+                Since the exchange needs to have only 1818, you can say that
+                2000 - 1818 = 182 can be sent back to Alice.
+              </div>
+              <div className="flex justify-center p-1 mt-5 mb-5">
+                <Image
+                  src="/images/pricing-uniswap-2.png"
+                  alt="Image"
+                  width={460}
+                  height={178}
+                />
+              </div>
+              <div className="mt-5">
+                In this way, the exchange can keep the original value of 20000
+                constant when they multiply the quantity of ETH and USDC.
+              </div>
+              <div className="mt-5">11 * 1818 = 20000</div>
+              <div className="mt-5">
+                Sure, 11 * 1818 is not exactly 20000, and financial math can not
+                be approximated in practice. But you get the basic idea. 🙂
+              </div>
+            </div>
+          )}
+          {currentQuestion < totalQuestions &&
+            currentQuestion !== 3 &&
+            currentQuestion !== 4 && (
+              <div className="flex flex-row justify-center items-center">
+                <Button className="mt-5" onClick={onContinue}>
+                  Continue
+                </Button>
+              </div>
+            )}
         </CardContent>
       </Card>
     </div>
---
+++ src/components/MultipleChoiceQuiz.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
+
+interface MultipleChoiceQuizProps {
+  className?: string;
+  question: string;
+  answers: string[];
+  correctAnswerIndex: number;
+  onCorrectAnswer: () => void;
+  onSkip?: () => void;
+}
+
+const MultipleChoiceQuiz: React.FC<MultipleChoiceQuizProps> = ({
+  className,
+  question,
+  answers,
+  correctAnswerIndex,
+  onCorrectAnswer,
+  onSkip,
+}) => {
+  const [selectedAnswer, setSelectedAnswer] = React.useState("");
+  const onAnswer = (value: string) => {
+    setSelectedAnswer(value);
+    if (value === answers[correctAnswerIndex]) {
+      onCorrectAnswer();
+    }
+  };
+
+  // track skipped state
+  const [skipped, setSkipped] = React.useState(false);
+
+  const onSkipped = () => {
+    if (skipped) {
+      return;
+    }
+    onSkip && onSkip();
+    setSkipped(true);
+  };
+
+  return (
+    <div className="flex flex-col items-center w-full">
+      <div className={cn("p-6 rounded-lg bg-[#F5F5F5] w-full", className)}>
+        <div className="text-base font-light mb-4">{question}</div>
+        <RadioGroup onValueChange={onAnswer}>
+          {answers.map((answer, index) => (
+            <div className="flex items-center space-x-2 p-1" key={index}>
+              <RadioGroupItem value={answer} id={index.toString()} />
+              <Label
+                htmlFor={index.toString()}
+                className="text-base font-light"
+              >
+                {answer}
+              </Label>
+            </div>
+          ))}
+        </RadioGroup>
+        {selectedAnswer === answers[correctAnswerIndex] ? (
+          <div className="mt-5">
+            <Label>🎉 Correct!</Label>
+          </div>
+        ) : (
+          selectedAnswer && (
+            <div className="mt-5">
+              <Label>Incorrect, please try again!</Label>
+            </div>
+          )
+        )}
+      </div>
+      <div
+        className="flex flex-row w-full justify-end items-center cursor-pointer"
+        onClick={onSkipped}
+      >
+        <div className="text-right mr-1 text-gray-400 font-bold">Skip</div>
+        <ChevronDown size={16} />
+      </div>
+    </div>
+  );
+};
+
+export default MultipleChoiceQuiz;
