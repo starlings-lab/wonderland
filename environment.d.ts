36afb76d32699239660e2fdd6ef9f4dac7a80dba
Tai <4382091+taisukemino@users.noreply.github.com>
2023-12-12 16:26:17 -0800

    more env variable type definition

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    TENDERLY_ACCESS_KEY: string;
-    NEXT_PUBLIC_TENDERLY_PROJECT: string;
     NEXT_PUBLIC_TENDERLY_USER: string;
+    NEXT_PUBLIC_TENDERLY_PROJECT: string;
+    TENDERLY_ACCESS_KEY: string;
+    NEXT_PUBLIC_TENDERLY_FORK_ID: string;
+    NEXT_PUBLIC_OWNER_ADDRESS: string;
   }
 }
