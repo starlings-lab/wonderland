a7d1b72999761a431bb2197cc425f45893dc9973
vnadoda <vipul@carapace.finance>
2023-12-13 14:37:02 -0600

    Fixed pre-rendering issue

--- src/app/dex/buying-selling/review/page.tsx
+++ src/app/dex/buying-selling/review/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 import ReviewCard from "@/components/ReviewCard";
--- src/app/dex/liquidity-provision/review/page.tsx
+++ src/app/dex/liquidity-provision/review/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import ReviewCard from "@/components/ReviewCard";
