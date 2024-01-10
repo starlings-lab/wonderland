5f5cdd6e24d273c3149d59bdc3690024a60e4faf
vnadoda <vipul@carapace.finance>
2024-01-10 16:56:48 -0600

    Center the homepage and added bun.lockb to gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,32 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle
+  CardTitle,
 } from "@/components/ui/card";
 
 export default function IndexPage() {
   return (
-    <Card className="ml-5 border-none shadow-none">
-      <CardHeader>
-        <CardTitle>
-          <p className="scroll-m-20 text-3xl tracking-tight home">
-            The best way to learn
-          </p>
-          <p className="scroll-m-20 text-3xl tracking-tight text-teal-500 home">
-            Crypto and DeFi
-          </p>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <CardDescription className="home-desc">
-          Learn proactively with interactive contents and playgrounds. 
-        </CardDescription>
-        <Button className="bg-[#179E7E] text-white mt-4">
-          <Link href={`/dex`}>Get Started</Link>
-        </Button>
-      </CardContent>
-    </Card>
+    <main className="flex flex-col items-center pt-16">
+      <Card className="border-none shadow-none">
+        <CardHeader>
+          <CardTitle>
+            <p className="scroll-m-20 text-3xl tracking-tight home">
+              The best way to learn
+            </p>
+            <p className="scroll-m-20 text-3xl tracking-tight text-teal-500 home">
+              Crypto and DeFi
+            </p>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="home-desc">
+            Learn proactively with interactive contents and playgrounds.
+          </CardDescription>
+          <Button className="bg-[#179E7E] text-white mt-4">
+            <Link href={`/dex`}>Get Started</Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
